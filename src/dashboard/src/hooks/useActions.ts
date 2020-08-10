a7be4904752bbb97bfd899f9a68666f26a5a53dd
zhangqilike2015 <zhangqilike@hotmail.com>
2020-08-10 10:34:44 +0800

    modify email template

--- src/dashboard/src/hooks/useActions.ts
+++ src/dashboard/src/hooks/useActions.ts
@@ -46,7 +46,11 @@ const useActions = (clusterId: string) => {
   }, [clusterId]);
 
   const onSupport = useCallback((event: any, job: any) => {
+<<<<<<< HEAD
     const subject = `[DLTS Job][${clusterId}][${job['vcName']}]: <Issue Title by User>`;
+=======
+    const subject = `[DLTS Job][${clusterId}][${job['vcName']}][${job['jobId']}]: <Issue Title by User>`
+>>>>>>> acee4c15... email template add job id (#1290)
     const body = `
 Hi DLTS support team,
 
