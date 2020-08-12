7eb77365285d4bf9c5f0635202dc29e2c038ec1f
zhangqilike2015 <zhangqilike@hotmail.com>
2020-08-12 08:31:33 +0800

    add notification for cluster

--- src/dashboard/src/Layout/NotificationBox.tsx
+++ src/dashboard/src/Layout/NotificationBox.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import {
+  Fragment,
   FunctionComponent,
   useContext,
+<<<<<<< HEAD
   useMemo,
 } from 'react';
 
 import { get } from 'lodash';
 
+=======
+  useMemo
+} from 'react'
+import useFetch from 'use-http-1'
+>>>>>>> 09d82aa5... Add cluster notification (#1291)
 import {
   Box,
   BoxProps,
@@ -14,6 +21,7 @@ import {
   Paper,
   Typography,
   createStyles,
+<<<<<<< HEAD
   makeStyles,
 } from '@material-ui/core';
 
@@ -21,6 +29,12 @@ import { Info } from '@material-ui/icons';
 
 import ConfigContext from '../contexts/Config';
 import TeamContext from '../contexts/Team';
+=======
+  makeStyles
+} from '@material-ui/core'
+import { Info } from '@material-ui/icons'
+import ClustersContext from '../contexts/Clusters'
+>>>>>>> 09d82aa5... Add cluster notification (#1291)
 
 const usePaperStyle = makeStyles(theme => createStyles({
   root: {
@@ -30,6 +44,7 @@ const usePaperStyle = makeStyles(theme => createStyles({
   },
 }));
 
+<<<<<<< HEAD
 const NotificationBox: FunctionComponent<BoxProps> = (props) => {
   const { notifications } = useContext(ConfigContext);
   const { currentTeamId } = useContext(TeamContext);
@@ -46,13 +61,46 @@ const NotificationBox: FunctionComponent<BoxProps> = (props) => {
 
   if (Boolean(notification) === false) return null;
 
+=======
+const ClusterNotifications: FunctionComponent<{ cluster: any }> = ({ cluster }) => {
+  const { data } = useFetch(`/api/clusters/${cluster.id}`, [cluster.id])
+  const notifications = useMemo(() => {
+    if (data === undefined) return []
+    if (!Array.isArray(data.notifications)) return []
+    return data.notifications as string[]
+  }, [data])
+  const paperStyle = usePaperStyle()
+  return (
+    <>
+      {
+        notifications.map((notification, index) => (
+          <Fragment key={index}>
+            <Paper elevation={0} classes={paperStyle}>
+              <Info fontSize="small" color="primary"/>
+              <Typography
+                variant="body2"
+                component={Box}
+                flex={1}
+                paddingLeft={1}
+                dangerouslySetInnerHTML={{ __html: notification }}
+              />
+            </Paper>
+            <Divider/>
+          </Fragment>
+        ))
+      }
+    </>
+  )
+}
+
+const NotificationBox: FunctionComponent<BoxProps> = (props) => {
+  const { clusters } = useContext(ClustersContext)
+>>>>>>> 09d82aa5... Add cluster notification (#1291)
   return (
     <Box {...props}>
-      <Paper elevation={0} classes={paperStyle}>
-        <Info fontSize="small" color="primary"/>
-        <Typography variant="body2" component={Box} flex={1} paddingLeft={1}>{notification}</Typography>
-      </Paper>
-      <Divider/>
+      {clusters.map(cluster => (
+        <ClusterNotifications key={cluster.id} cluster={cluster}/>
+      ))}
     </Box>
   );
 };
